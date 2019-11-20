cd811a2ed5af6801fe663f02068cdece950ad4cb
jasmosez <jas.schaffer@gmail.com>
2019-11-20 16:58:59 -0500

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,19 @@
-# Your code goes here!
+require 'pry'
+
+class Anagram
+
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(possible_anagrams)
+    #return all matches in an array. If no matches exist, it should return an empty array.
+    possible_anagrams.select do |gram|
+      gram.split("").sort == @word.split("").sort
+    end
+  end
+
+end
+
